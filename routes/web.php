47b5f1d58b2203d225d6300c1deaaf7241bbdf46
Ibrahim <61706383+ibrahim-yavuz@users.noreply.github.com>
2022-09-02 12:34:18 +0300

    add null check to main url

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ use Illuminate\Support\Facades\Storage;
 
 Route::get('/', function () {
     $dailyPlayer = \App\Models\DailyPlayer::latest()->first();
+    if($dailyPlayer == null){
+        return "Couldn' find Player";
+    }
     return Player::with('country.logo', 'team.logo', 'roles', 'game', 'logo')->find($dailyPlayer->player_id);
 });
 
