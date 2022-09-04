85f3152551400aaca84ab8338cb61c3cef1a8224
Ibrahim <61706383+ibrahim-yavuz@users.noreply.github.com>
2022-09-04 16:40:55 +0300

    add post method to api file

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Country;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,14 +25,21 @@ Route::get('filter-players/', function (){
     return [];
 });
 
-Route::post('upload-image', function (Request $request){
-    $file_name = $request->file('logo')->getClientOriginalName();
-    $type = $request->type;
-    $uploaded_path = 'logos/'.$type."/";
-    $request->file('logo')->storeAs("public/".$uploaded_path, $file_name);
+//Route::post('upload-image', function (Request $request){
+//    $file_name = $request->file('logo')->getClientOriginalName();
+//    $type = $request->type;
+//    $uploaded_path = 'logos/'.$type."/";
+//    $request->file('logo')->storeAs("public/".$uploaded_path, $file_name);
+//
+//    $full_path_of_logo = asset("storage/".$uploaded_path.$file_name);
+//    $logo = new \App\Models\Logo();
+//    $logo->logo_url = $full_path_of_logo;
+//    $logo->save();
+//});
 
-    $full_path_of_logo = asset("storage/".$uploaded_path.$file_name);
-    $logo = new \App\Models\Logo();
-    $logo->logo_url = $full_path_of_logo;
-    $logo->save();
+Route::post('countries', function (Request $request){
+    $country = new Country();
+    $country->name = $request->name;
+    $country->logo_id = $request->logo_id;
+    $country->save();
 });
