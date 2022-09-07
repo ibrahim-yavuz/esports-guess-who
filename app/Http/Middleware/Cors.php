229203e250ccd2d1fe959547a97cfa090cdbbb5f
Ibrahim <61706383+ibrahim-yavuz@users.noreply.github.com>
2022-09-08 00:05:38 +0300

    remove cors package

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,8 +16,8 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        header('Access-Control-Allow-Origin');
-        header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
+        header('Access-Control-Allow-Origin: *');
+        //header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
         header('Access-Control-Allow-Methods:  POST, PUT, GET');
         return $next($request);
     }
