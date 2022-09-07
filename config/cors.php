2b3a1ea3b6b1bcf1a842d6fcaa360fa0ac1777a0
Ibrahim <61706383+ibrahim-yavuz@users.noreply.github.com>
2022-09-07 17:38:51 +0300

    add a function to update logo urls to new server

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [''],
+    'allowed_origins' => ['https://esports-guess-who-flutter.web.app'],
 
-    'allowed_origins_patterns' => ['https://esports-guess-who-flutter.web.app'],
+    'allowed_origins_patterns' => ['*'],
 
     'allowed_headers' => ['*'],
 
