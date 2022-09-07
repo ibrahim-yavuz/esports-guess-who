326520ab9505d534d60afc3eec3249ada3f2b199
Ibrahim <61706383+ibrahim-yavuz@users.noreply.github.com>
2022-09-07 23:50:30 +0300

    remove cors package

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,7 +16,7 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        header('Access-Control-Allow-Origin: ');
+        header('Access-Control-Allow-Origin: https://esports-guess-who-flutter.web.app/#/Home');
         header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
         header('Access-Control-Allow-Methods:  POST, PUT, GET');
         return $next($request);
